b9d6d00260ca7eb19e68c00d7286dd3c0f9c9178
Adam Kvant <kvant003@umn.edu>
2024-12-18 20:24:08 -0600

    setting up header component

---
+++ AdamKvantSite/src/components/Header.svelte
@@ -0,0 +1,6 @@
+<div class="headerOverall">
+    <span class="hoverHeader">Home</span> |
+    <span class="hoverHeader">Experience</span> |
+    <span class="hoverHeader">Projects</span> |
+    <span class="hoverHeader">Contact Me</span>
+</div>
