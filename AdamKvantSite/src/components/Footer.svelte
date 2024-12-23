3a73bf8a7bc32f46b9e8f678bb348caaaceaedb9
Adam Kvant <kvant003@umn.edu>
2024-12-23 16:59:43 -0600

    Added footer

---
+++ AdamKvantSite/src/components/Footer.svelte
@@ -0,0 +1,2 @@
+<div class="w-full headerOverall rounded-t-lg">
+</div>
--- AdamKvantSite/src/routes/+layout.svelte
+++ AdamKvantSite/src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import '../app.css';
     import Header from '../components/Header.svelte';
+    import Footer from '../components/Footer.svelte'
 	let { children } = $props();
 </script>
 
 <div class="flex justify-center">
-    <div class="sm:w-11/12 lg:w-2/3 flex flex-col">
+    <div class="sm:w-11/12 lg:w-9/12 flex flex-col">
         <Header/>
         <div>{@render children()}</div>
+        <Footer/>
     </div>
 </div>
 
