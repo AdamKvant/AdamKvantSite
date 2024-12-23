5f2c4120a43ecd371474cc381615cdd3e71d29dd
Adam Kvant <kvant003@umn.edu>
2024-12-22 18:12:02 -0600

    streamlined Card component, starting tags next time

---
+++ AdamKvantSite/src/components/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let title = "Default Title";
+    export let description = "Default description";
+</script>
+  
+  <div class="Card">
+    <h1 class="CardTitle">{title}</h1>
+    <div class="CardDesc">
+      {@html description}
+    </div>
+  </div>
--- AdamKvantSite/src/routes/+page.svelte
+++ AdamKvantSite/src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { onMount } from 'svelte';
-  
+        import Card from '../components/Card.svelte'
     onMount(() => {
       document.title = "Adam Kvant";
     });
@@ -10,7 +10,7 @@
 <div class="flex flex-col items-center gap-3 min-h-full">
 
   <!-- Intro -->
-  <div class="Intro" id="Intro">
+  <div class="Card Intro" id="Intro">
       <img src="/images/Headshot.jpg" alt="Adam Kvant Headshot" class="headshot">
     <div class="flex flex-col w-4/5 sm:w-full">
       <h1 class="Name">Adam Kvant</h1>
@@ -24,8 +24,17 @@
   <!-- End of Intro -->
 
   <!-- About Me -->
-  <div class="Card">
-    Hello
-  </div>
+  <Card title="About Me"
+  description="Hello! I'm Adam, I recently graduated with distinction from UMN's College of Science and and Engineering with a B.S. in Computer Science.
+      I enjoy learning all things technology, whether it's software or hardware. I have interned in various sectors of the tech industry (IT, Data Science, and Development),
+      and these roles have provided me with unique and useful skills that I can apply in any setting.
+      <br>
+      Outside of work, I enjoy learning new tech stacks (I learned Svelte for this site) and 3D printing.
+      I also spend my time lifting, baking bread, fencing (Sabre), rock climbing, gaming, and playing soccer." />
   <!-- End About Me -->
+
+  <!-- Skills -->
+  <Card title="Skills"
+  description="Blah"/>
+  <!-- End Skills -->
 </div>
