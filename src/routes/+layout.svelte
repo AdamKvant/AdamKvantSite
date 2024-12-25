f8efa6de37f233bf7d149f9b28329b5de9da73d8
Adam Kvant <kvant003@umn.edu>
2024-12-25 12:09:30 -0600

    restructure dir

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+    import Header from '../components/Header.svelte';
+    import Footer from '../components/Footer.svelte'
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex justify-center">
+    <div class="sm:w-11/12 lg:w-9/12 flex flex-col">
+        <Header/>
+        <div>{@render children()}</div>
+        <Footer/>
+    </div>
+</div>
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}','./index.html'],
+	
 
 	theme: {
 		extend: {}
