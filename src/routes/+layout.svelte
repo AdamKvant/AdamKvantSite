8c77a1163ce2f528eb6c6d08239027c1871cb695
Adam Kvant <kvant003@umn.edu>
2024-12-30 01:21:41 -0600

    No more overscroll mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="flex justify-center">
-    <div class="sm:w-11/12 lg:w-9/12 flex flex-col min-h-screen relative">
+    <div class="sm:w-11/12 lg:w-9/12 flex flex-col min-h-screen relative overscroll-none">
         <Header/>
         <div class="mb-16">{@render children()}</div>
         <div class="absolute bottom-0 w-full">
