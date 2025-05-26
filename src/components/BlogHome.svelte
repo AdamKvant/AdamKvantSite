17ef429fbeeea75d144d21b7f55e2971691a0024
AdamKvant <adamkvant@gmail.com>
2025-05-26 17:39:45 -0500

    added base home layout for blogs

---
+++ src/components/BlogHome.svelte
@@ -0,0 +1,66 @@
+<script>
+    export let blogs = [
+        {
+          title: "PDAParser",
+          description: "",
+          tags: ["Python","File I/O","Medical Research Contribution"],
+          disclaimer:false,
+          source: "Final_Study_Poster.png",
+          alt: "Study Poster",
+          id: 0,
+      },
+    ];
+    import Tag from "./Tag.svelte";
+  </script>
+  
+  <div class="flex flex-col space-y-4 w-full items-center my-6">
+    <div class="flex items-center justify-center text-2xl font-bold text-[#FCFAEE] px-6 pt-2 Card md:w-fit shadow-2xl">
+      <span class="-pb-3 lg:-mb-4 lg:-mt-1.5">All project cards are links to GitHub or other related resources.</span>
+    </div>
+    {#each blogs as item, index}
+      <a href={`blog/${item.id}`} class="Card flex flex-col-reverse items-center shadow-2xl">
+        {#if item.source}
+        <div class="space-y-1.5 w-full px-6 mt-2">
+          <h2 class="text-2xl font-bold text-[#FCFAEE]">{item.title}</h2>
+          {#if item.description}
+            <p>{item.description}
+              {#if item.disclaimer}
+                <br>
+                <span class="italic text-[#B8001F] text-sm">May not be hosted anymore</span>
+              {/if}
+            </p>
+          {/if}
+          <div class="flex flex-wrap justify-center gap-2 pb-3 lg:pb-1">
+            {#each item.tags as tag}
+              <Tag text={tag}/>
+            {/each}
+          </div> 
+        </div>
+        {/if}
+        {#if !item.source}
+          <div class="space-y-1.5 w-full px-6 mt-2">
+            <h2 class="text-2xl font-bold text-[#FCFAEE] mt-2">{item.title}</h2>
+            {#if item.description}
+              <p>{item.description}
+                {#if item.disclaimer}
+                  <br>
+                  <span class="italic text-[#B8001F] text-sm">May not be hosted anymore</span>
+                {/if}
+              </p>
+            {/if}
+            <div class="flex flex-wrap justify-center gap-2 pb-3 lg:pb-1">
+              {#each item.tags as tag}
+                <Tag text={tag}/>
+              {/each}
+            </div> 
+          </div>
+        {/if}
+        {#if item.source}
+        <div class="w-full pt-6 px-6">
+          <img class="rounded-xl shadow-lg" src="images/{item.source}" alt={item.alt}>
+        </div>
+        {/if}
+      </a>
+    {/each}
+  </div>
+  
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    onMount(() => {
+      document.title = "Blog";
+    });
+    import BlogHome from '../../components/BlogHome.svelte';
+  </script>
+  <BlogHome/>
