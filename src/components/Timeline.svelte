8f42f4f1c350dcd7f034bcbe22ed52bb71f53f1e
AdamKvant <adamkvant@gmail.com>
2025-06-17 21:48:06 -0500

    fixed some formatting and reduced coupling on proj/ex

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,104 +1,33 @@
 <script>
-    export let timeline = [
-      {
-        title: "Application Engineering Associate",
-        company: "RSM US LLP",
-        duration: "January 2025 - Present",
-        description: [
-          "Earned the fastest Microsoft PL-900 and PL-200 certifications after joining the firm (both in my first 3 months).",
-        ],
-        source:"rsm_us_llp_logo.jpg",
-        alt:"RSM Logo",
-    },
-    {
-        title: "Application Engineering Intern",
-        company: "RSM US LLP",
-        duration: "June 2024 - August 2024",
-        description: [
-          "Developed Model-Driven Apps, Canvas Apps, Power Automate Flows, Power Pages, and other Power Platform tools.",
-          "Generated web resources in JavaScript to automate and customize Power Platform application frontends.",
-          "Generated plugins and scripts in C# to automate backend Power Platform application processes.",
-          "Utilized OData API requests to implement custom data filtering, tested using Postman."
-        ],
-        source:"rsm_us_llp_logo.jpg",
-        alt:"RSM Logo",
-    },
-    {
-      title:"CSCI 1933 Course Content Designer",
-      company:"UMN Department of Computer Science & Engineering",
-      duration: "May 2024 - June 2024",
-      description:[
-        "Took initiative to gain support from the course professor and department to secure funding.",
-        "Led a major course-wide restructuring of course content, the changes affect ~400 students each semester.",
-        "Specific details can be found in the \"CSCI 1933 Course Overhaul\" project in my \"Projects\" page.",
-      ],
-      source:"umncs.jpg",
-      alt:"University of Minnesota Computer Science Logo",
-    },
-    {
-        title: "Undergraduate Teaching Assistant",
-        company: "UMN Department of Computer Science & Engineering",
-        duration: "September 2023 - December 2024",
-        description: [
-          "Undergraduate Teaching Assistant for CSCI 1933: Introduction to Algorithms and Data Structures",
-          "Project Leader: I directed the creation of many major assignments that introduce core Java concepts.",
-          "I lead a lab section with two other TAs, answering questions and directing lab overview.",
-        ],
-        source:"umncs.jpg",
-        alt:"University of Minnesota Computer Science Logo",    
-    },
-    {
-        title: "Data Science Intern",
-        company: "Optum",
-        duration: "June 2023 - August 2023",
-        description: [
-          "Researched Clinical Trial Optimization Feasibility for OptumAI.",
-          "Implemented SQL queries on insurance claims database to generate synthetic control arms for three clinical trials.",
-          "Supported initial progress towards two publications.",
-          "Presented to senior engineers and executives in Shark Tank presentations and managed stakeholder expectations."
-        ],
-        source:"optum.jpg",
-        alt:"Optum Logo",    
-    },
-    {
-        title: "Systems Support Analyst Intern",
-        company: "Egan Company",
-        duration: "May 2022 - August 2022",
-        description: [
-          "Led a summer long project of laptop maintenance, repairs, and updates for various Windows machines.",
-          "Collaborated with other interns on replacing employee phones throughout the metro area.",
-          "Learned how to virtualize apps, use Cylance software, Active Directory, and some server maintenance.",
-        ],
-        source:"egan.jpg",
-        alt:"Egan Logo",    
-    },
-    ];
-  </script>
-  
-  <div class="flex flex-col space-y-4 w-full items-center my-6">
-    {#each timeline as item}
-      <div class={`Card shadow-2xl pt-2 ${item.description.length == 0 ? 'md:pb-2' : 'md:pb-4'}`}>
-        <div class="md:mt-2 md:mb-0 md:ml-8 space-y-2 md:mr-4 mx-7">
-          <div class="flex flex-col md:flex-row items-center md:items-start">
-            <img class="shadow-lg rounded-full h-16 w-16 md:h-14 md:w-14 md:mr-2 mt-2 md:mt-0 mb-3 md:mb-0" src="/images/{item.source}" alt={item.alt}>
-            <div class="flex flex-col items-center md:items-start">
-              <h2 class="text-lg font-bold text-[#FCFAEE]">{item.title}</h2>
-              <div class="flex flex-row items-center md:items-start gap-3">
-                <span class="text-sm md:text-base font-bold text-gray-300">{item.company}</span>
-                <span class="text-sm md:text-base text-gray-300 italic">{item.duration}</span>
-              </div>
-            </div>
-            
-          </div>
-          {#if item.description}
-          <ul class=" mx-4 list-disc pb-2 md:pb-0">
-            {#each item.description as desc}
-            <li class="text-sm md:text-base">{desc}</li>
-            {/each}
-          </ul>
-          {/if}
-        </div>
-      </div>
-    {/each}
-  </div>
-  
+	export let timeline;
+</script>
+
+<div class="my-6 flex w-full flex-col items-center space-y-4">
+	{#each timeline as item}
+		<div class={`Card pt-2 shadow-2xl ${item.description.length == 0 ? 'md:pb-2' : 'md:pb-4'}`}>
+			<div class="mx-7 space-y-2 md:mb-0 md:ml-8 md:mr-4 md:mt-2">
+				<div class="flex flex-col items-center md:flex-row md:items-start">
+					<img
+						class="mb-3 mt-2 h-16 w-16 rounded-full shadow-lg md:mb-0 md:mr-2 md:mt-0 md:h-14 md:w-14"
+						src="/images/{item.source}"
+						alt={item.alt}
+					/>
+					<div class="flex flex-col items-center md:items-start">
+						<h2 class="text-lg font-bold text-[#FCFAEE]">{item.title}</h2>
+						<div class="flex flex-row items-center gap-3 md:items-start">
+							<span class="text-sm font-bold text-gray-300 md:text-base">{item.company}</span>
+							<span class="text-sm italic text-gray-300 md:text-base">{item.duration}</span>
+						</div>
+					</div>
+				</div>
+				{#if item.description}
+					<ul class=" mx-4 list-disc pb-2 md:pb-0">
+						{#each item.description as desc}
+							<li class="text-sm md:text-base">{desc}</li>
+						{/each}
+					</ul>
+				{/if}
+			</div>
+		</div>
+	{/each}
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,80 @@
 <script>
-    import { onMount } from 'svelte';
-  
-    onMount(() => {
-      document.title = "Projects";
-    });
+	export let projects = [
+		{
+			title: 'PDAParser',
+			description:
+				"PDAParser is a quaint CLI program that I created to aid a life-long friend, Jackson Watson D.O.'28, in processing data from his medical research lab. This program normalizes values from raw EMG data on muscles in the foot and lower leg, and puts them in an easily readable output file. This program was straightforward to create, and saved significant time in processing data from the lab. Information about this study is in the README found by clicking on this tile.",
+			tags: ['Python', 'File I/O', 'Medical Research Contribution'],
+			disclaimer: false,
+			source: 'Final_Study_Poster.png',
+			alt: 'Study Poster',
+			link: 'https://github.com/AdamKvant/PDAParser'
+		},
+		{
+			title: 'adamkvant.dev',
+			description:
+				"This site was created to learn more about component-based frameworks and to pick up Svelte. The site follows the standard SPA structure, and it is hosted on Google Firebase. The color scheme is also based upon my Rocket League car's color.",
+			tags: ['Svelte', 'TailwindCSS', 'JavaScript', 'TypeScript', 'Firebase'],
+			disclaimer: false,
+			source: 'AKSitePic.png',
+			alt: 'adamkvant.dev picture',
+			link: 'https://github.com/AdamKvant/AdamKvantSite'
+		},
+		{
+			title: 'Globetrotter',
+			description:
+				'A mobile friendly, trip logging app created using EmberJS, TailwindCSS, and hosted on Google Firebase. Users can upload trips through the destination selector, and can view them on the home page or the timeline. The timeline also has a dynamic filter option to filter based on various trip properties.',
+			tags: ['EmberJS', 'TailwindCSS', 'Figma', 'Firebase', 'Firestore'],
+			disclaimer: true,
+			source: 'Globetrotter.png',
+			alt: 'Globetrotter picture',
+			link: 'https://github.com/csci5117f24/project-2-peanut-butter-pickles'
+		},
+		{
+			title: 'CulinaryCards',
+			description:
+				'A recipe sharing application without the ads and short stories. The site features full-text search, and the ability to like and save recipes to the user profile.',
+			tags: ['Flask', 'JavaScript', 'PostgreSQL', 'PureCSS'],
+			disclaimer: true,
+			source: 'CulinaryCards.png',
+			alt: 'CulinaryCards picture',
+			link: 'https://github.com/csci5117f24/project-1-peanut-butter-pickles'
+		},
+		{
+			title: 'CSCI 1933 Course Overhaul',
+			description: `This was a month long summer project consisting of work updating all course related material for CSCI 1933, a required class for pre-CS majors at UMN. Samantha and I took initiative by getting this project approved by the professor and department for the summer.
+      We converted all LaTeX documents over to markdown files with their own repo and created Gradescope autograders for all applicable projects. We also set up a new GitHub organization for students and course staff to interact with.
+      This meant we updated all labs, projects, and their related rubrics, unit tests, and code.`,
+			tags: ['Java', 'JUnit', 'GitHub', 'Bash', 'Algorithms & Data Structures', 'Documentation'],
+			disclaimer: false,
+			link: 'https://www.linkedin.com/in/adamkvant/details/projects/'
+		},
+		{
+			title: 'MathWhiz',
+			description: `MathWhiz is a minimalist web-app that I created over the span of a couple of days in undergrad.
+      The user tries to get as many random arithmetic questions right within one minute.`,
+			tags: ['ExpressJS', 'CSS', 'JavaScript', 'PUG'],
+			source: 'MathWhiz.png',
+			alt: 'MathWhiz picture',
+			disclaimer: true,
+			link: 'https://github.com/AdamKvant/MathWhiz'
+		},
+		{
+			title: 'Card Games in C#',
+			description: `This project is an extendable platform to create card games in the C# console. I've programmed all of this project in C# and I plan on adding new card games to this project in the future. Some other future ideas for extensibility that I am thinking about are: creating a website, or a desktop app with a GUI. Right now, this program only has Blackjack implemented, and the code for Blackjack is fully documented using Doxygen and a UML Class Diagram.`,
+			tags: ['C#', 'Doxygen', 'UML'],
+			disclaimer: false,
+			link: 'https://github.com/AdamKvant/CardGamesCSharp'
+		}
+	];
 
-    import Projects from '../../components/Projects.svelte';
-  </script>
-  <Projects/>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.title = 'Projects';
+	});
+
+	import Projects from '../../components/Projects.svelte';
+</script>
+
+<Projects projects={projects}/>
