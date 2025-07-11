3262b23f1c9a49295f91d2839fa8fd623629a541
AdamKvant <adamkvant@gmail.com>
2025-07-11 11:43:25 -0500

    disabled blog as it broke on deployment

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -27,7 +27,7 @@
     <div class="hidden md:flex flex-row gap-4 pr-5">
         <a class="hoverHeader" href="/experience">Experience</a>
         <a class="hoverHeader" href="/projects">Projects</a>
-        <a class="hoverHeader" href="/blog">Blog</a>
+        <!-- <a class="hoverHeader" href="/blog">Blog</a> -->
     </div>
 
     <!-- Dropdown Menu -->
@@ -35,6 +35,6 @@
         transform ${isOpen ? 'translate-y-0 opacity-100' : '-translate-y-2 opacity-0'}`}>
         <a on:click={toggleMenu} class="hoverHeaderHam w-full text-right border border-b-4 border-x-0 border-t-0 pb-1.5 z-0" href="/experience">Experience</a>
         <a on:click={toggleMenu} class="hoverHeaderHam w-full text-right border border-b-4 border-x-0 border-t-0 pb-1.5 z-0" href="/projects">Projects</a>
-        <a on:click={toggleMenu} class="hoverHeaderHam w-full text-right border border-b-4 border-x-0 border-t-0 pb-1.5 z-0" href="/blog">Blog</a>
+        <!-- <a on:click={toggleMenu} class="hoverHeaderHam w-full text-right border border-b-4 border-x-0 border-t-0 pb-1.5 z-0" href="/blog">Blog</a> -->
     </div>
 </div>
