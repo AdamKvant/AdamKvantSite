29518919d9f9407683fedd27edd53555aecfa450
AdamKvant <adamkvant@gmail.com>
2025-09-06 15:02:54 -0500

    updated header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import '../app.css';
     import Header from '../components/Header.svelte';
-    import Footer from '../components/Footer.svelte'
+    import Footer from '../components/Footer.svelte';
+    import IconHeader from '../components/IconHeader.svelte';
 	let { children } = $props();
 </script>
 
 <div class="flex flex-col min-h-screen items-center">
+    <IconHeader/>
     <div class="sm:w-11/12 lg:w-10/12 w-full"><Header /></div>
     <div class="flex justify-center flex-grow w-full">
         <div class="aspect-h-9 aspect-w-16">
